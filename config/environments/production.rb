287d11cf8639f88037e53c58f69e37de8665ccdf
maskwerewolf <maskwerewolf@163.com>
2014-01-21 10:17:23 +0800

    fuck you

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ LineUp::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  #config.i18n.fallbacks = true
+  #config.i18n.default_locale = :zh
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
@@ -65,6 +66,6 @@ LineUp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.i18n.default_locale = :zh
+
   config.time_zone = 'Beijing'
 end
